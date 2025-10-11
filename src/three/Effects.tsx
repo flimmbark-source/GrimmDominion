2c335d783257bf116c70e6b63c8c06386fcf5a05
flimmbark-source <flimmbark@gmail.com>
2025-10-11 06:28:09 -0700

    Create folder src/three with placeholder 3D component files.

---
+++ src/three/Effects.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const Effects = (): null => {
+  useEffect(() => {
+    console.log("Effects3D mounted");
+  }, []);
+
+  return null;
+};
+
+export default Effects;
---
+++ src/three/World3D.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const World3D = (): null => {
+  useEffect(() => {
+    console.log("World3D mounted");
+  }, []);
+
+  return null;
+};
+
+export default World3D;
